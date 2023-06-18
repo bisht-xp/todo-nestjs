47db019829f5485ce0ee0a6f1cd640497277d681
bisht-xp <bishtkamal2907@gmail.com>
2023-06-18 14:40:12 +0530

    Update: update the code

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TodoModule } from './todo/todo.module';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    TodoModule,
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGO_URL),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
