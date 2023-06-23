b9e488d646ee14210186f7ea8b4320ce628cd724
bisht-xp <bishtkamal2907@gmail.com>
2023-06-23 16:37:23 +0530

    add: console log

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
+  const logger = new Logger();
   const app = await NestFactory.create(AppModule);
   await app.listen(8080);
+  logger.log(`Application is running on: ${await app.getUrl()}`);
+  console.log(`Server is Running`)
 }
 bootstrap();
