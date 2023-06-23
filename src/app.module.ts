f8d62015740505fbf06eb29b2184266c8f0882ff
bisht-xp <bishtkamal2907@gmail.com>
2023-06-23 16:39:12 +0530

    Add: added auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,14 @@ import { AppService } from './app.service';
 import { TodoModule } from './todo/todo.module';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     TodoModule,
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.MONGO_URL),
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
