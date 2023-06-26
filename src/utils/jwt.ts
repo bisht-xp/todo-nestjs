5630d0c1314f70631105a603a0057c48f309b330
bisht-xp <bishtkamal2907@gmail.com>
2023-06-26 18:54:59 +0530

    create: created new jwt file

---
+++ src/utils/jwt.ts
@@ -0,0 +1,35 @@
+import * as jwt from 'jsonwebtoken';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+export const EXPIRATION_TIME = '1h';
+
+console.log(process.env.JWT_SECRET_KEY);
+export const generateAccessToken = (data: {
+  email?: string;
+  address?: string;
+  userId: string;
+}) => {
+  const jwtPayload = {
+    email: data.email ? data.email : '',
+    address: data.address ? data.address : '',
+    userId: data.userId,
+  };
+
+  return jwt.sign(jwtPayload, process.env.JWT_SECRET_KEY, {
+    expiresIn: EXPIRATION_TIME,
+  });
+};
+
+export const verifyAccessToken = async (token: string) => {
+  try {
+    const data = await jwt.verify(token, process.env.JWT_SECRET_KEY);
+    if (data) {
+      return data;
+    }
+    return null;
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
+};
