d1d2bdfe5ee6c9cfa2894a12b35401b98799ec6d
bisht-xp <bishtkamal2907@gmail.com>
2023-06-29 18:23:50 +0530

    Add: added jwt verification

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,20 +1,27 @@
-import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
+import { Body, Controller, Get, Headers, Post, Put } from '@nestjs/common';
 import { newTask } from './todo.dto';
 import { TodoService } from './todo.service';
+import { verifyAccessToken } from 'src/utils/jwt';
 
 @Controller('')
 export class TodoController {
   constructor(private todoService: TodoService) {}
 
-  @Post('v1/add')
-  addTask(@Body() data: newTask) {
-    return this.todoService.createTodo(data);
+  @Post('v1/add/task')
+  async addTask(@Headers() headers: any, @Body() task: newTask) {
+    try {
+      const token = headers['x-access-token'];
+      const data = await verifyAccessToken(token);
+      return this.todoService.createTodo(task, data.userId);
+    } catch (error) {
+      throw error;
+    }
   }
 
-  // @Get('v1/all')
-  // showAllTask() {
-  //   return this.todoService.allTodos();
-  // }
+  @Get('v1/all')
+  showAllTask() {
+    return this.todoService.allTodos();
+  }
 
   // @Put('v1/update/:id')
   // update(@Param() id) {
