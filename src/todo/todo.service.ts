b8ff7a3287b286b0cf9af90e90974aa26bd13ad0
bisht-xp <bishtkamal2907@gmail.com>
2023-07-01 18:29:17 +0530

    Add: added delete and update todo function

--- src/todo/todo.service.ts
+++ src/todo/todo.service.ts
@@ -1,9 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import * as dotenv from 'dotenv';
 import { InjectModel } from '@nestjs/mongoose';
 import { Todo } from 'src/model/todo.model';
 import { newTask } from 'src/utils/constants';
 import { Model } from 'mongoose';
+import { updateTodo } from './todo.dto';
 dotenv.config();
 
 @Injectable()
@@ -11,34 +12,54 @@ export class TodoService {
   constructor(@InjectModel(Todo.name) private todoModel: Model<Todo>) {}
 
   async createTodo(data: newTask, userID: string) {
-    let createTask = {
-      ...data,
-      userID: userID,
-    };
-    let task = new this.todoModel(createTask);
-    await task.save();
-    return {
-      status: true,
-      message: 'new task created successfully...',
-      task,
-    };
+    try {
+      let task = {
+        ...data,
+        userID: userID,
+      };
+      let newTask = new this.todoModel(task);
+      await newTask.save();
+      return {
+        status: true,
+        message: 'new task created successfully...',
+        newTask,
+      };
+    } catch (error) {
+      throw error;
+    }
   }
 
-  allTodos() {
+  getAllTodos() {
     const todos = this.todoModel.find().populate('userID');
     return todos;
   }
 
-  // patchTodo(id) {
-  //   console.log(id.id);
-  //   // console.log(this.todos);
-  //   for (let todo of this.todos) {
-  //     // console.log(typeof todo._id);
-  //     if (todo._id === id.id) {
-  //       console.log(todo);
-  //       todo.isCompleted = true;
-  //     }
-  //   }
-  //   return 'fixed';
-  // }
+  async updateTask(id: string, data: updateTodo) {
+    try {
+      let task = await this.todoModel.findById({ _id: id });
+      if (!task) {
+        throw new ForbiddenException('There is no task to update');
+      }
+      await this.todoModel.updateOne({ _id: id }, { $set: { ...data } });
+      return {
+        status: 'true',
+        message: 'Successfully update',
+        task,
+      };
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async deleteTask(id: string) {
+    try {
+      let task = await this.todoModel.findByIdAndDelete({ _id: id });
+      return {
+        status: 'true',
+        message: 'Successfully Deleted',
+      };
+    } catch (error) {
+      throw error;
+    }
+  }
 }
